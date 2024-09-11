b887eb26cdb9ed184a142d875ed0532a18c02123
marimbotico <isaacfh@bu.edu>
2024-09-10 20:00:27 -0400

    imported logos

--- src/components/About/codeLogos.jsx
+++ src/components/About/codeLogos.jsx
@@ -1,8 +1,13 @@
+import Bootstrap from '../assets/bootstrap.png';
+import JS from '../assets/javascript.jpg';
+import CSS from '../assets/css.jpg';
+import HTML from '../assets/html-5.jpg';
+import React from '../assets/react.jpg';
 
 export const codeLogos = [
-  { name: "JS", src: "/assets/javascript.jpg", alt: "JavaScript" },
-  { name: "HTML", src: "/assets/html-5.jpg", alt: "HTML" },
-  { name: "CSS", src: "/assets/css.jpg", alt: "CSS" },
-  { name: "Bootstrap", src: "/assets/bootstrap.png", alt: "Bootstrap" },
-  { name: "React", src: "/assets/react.jpg", alt: "React" },
+  { name: "JS", src: JS, alt: "JavaScript" },
+  { name: "HTML", src: HTML, alt: "HTML" },
+  { name: "CSS", src: CSS, alt: "CSS" },
+  { name: "Bootstrap", src: Bootstrap, alt: "Bootstrap" },
+  { name: "React", src: React, alt: "React" },
 ];

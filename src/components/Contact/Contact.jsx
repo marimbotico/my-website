a4938460e2ff76386be2a81ceeef05071dbf52e9
marimbotico <isaacfh@bu.edu>
2024-09-13 12:27:56 -0400

    fixed aws pdf rewrite

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import { Bounce } from 'react-awesome-reveal';
 import Github from './assets/github.jpg';
 import Linkedin from './assets/linkedin.jpg';
 import Resume from './assets/resume.jpg';
-import ResumePDF from '../Contact/assets/ResumePDF.pdf';
+import ResumePDF from './assets/ResumePDF.pdf';
 
 
 
